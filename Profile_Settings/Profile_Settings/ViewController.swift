6cb0701105ba3781a5fd27fb8304f35cfbe40086
Michal Pietrzak <michal.pietrzak18@gmail.com>
2020-12-24 13:40:43 +0100

    Initial project setup

---
+++ Profile_Settings/Profile_Settings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Profile_Settings
+//
+//  Created by Michal Pietrzak on 24/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
