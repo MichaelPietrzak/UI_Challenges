1329645a1de6ce0dee34cc051830f882d16d575f
Michal Pietrzak <michal.pietrzak18@gmail.com>
2020-12-25 15:24:27 +0100

    adding color Palette + styling navBar

--- Profile_Settings/Profile_Settings/ProfileViewController.swift
+++ Profile_Settings/Profile_Settings/ProfileViewController.swift
@@ -11,5 +11,39 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "Profile"
+        
+        // MARK: - Color Palette
+        let darkColor = UIColor(red: 0.04, green: 0.04, blue: 0.04, alpha: 1.00)
+        let whiteColor = UIColor(red: 0.78, green: 0.78, blue: 0.78, alpha: 1.00)
+        let grayColor = UIColor(red: 0.21, green: 0.21, blue: 0.21, alpha: 1.00)
+        let greenColor = UIColor(red: 0.34, green: 0.58, blue: 0.46, alpha: 1.00)
+        
+        view.backgroundColor = darkColor
+        
+        // MARK: - Navigation Bar
+        self.navigationController?.navigationBar.tintColor = whiteColor
+        self.navigationController?.navigationBar.barTintColor = darkColor
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: whiteColor, NSAttributedString.Key.font: UIFont(name: "Futura-Bold", size: 20)!]
+        
+        let imageConfiguration = UIImage.SymbolConfiguration(pointSize: 20, weight: .black)
+        let menuButtonImage = UIImage(systemName: "text.alignleft", withConfiguration: imageConfiguration)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: menuButtonImage, style: .plain, target: nil, action: nil)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: nil)
+        navigationItem.rightBarButtonItem?.tintColor = whiteColor
+        navigationItem.rightBarButtonItem?.setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "Futura-Bold", size: 20)!], for: UIControl.State.normal)
+        
+        let backButtonImage = UIImage(systemName: "chevron.backward", withConfiguration: imageConfiguration)
+        
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = whiteColor
+        
+        self.navigationController?.navigationBar.backIndicatorImage = backButtonImage
+        self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = backButtonImage
     }
 }
--- Profile_Settings/Profile_Settings/SettingsViewController.swift
+++ Profile_Settings/Profile_Settings/SettingsViewController.swift
@@ -11,5 +11,15 @@ class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "Settings"
+        
+        // MARK: - Color Palette
+        let darkColor = UIColor(red: 0.04, green: 0.04, blue: 0.04, alpha: 1.00)
+        let whiteColor = UIColor(red: 0.78, green: 0.78, blue: 0.78, alpha: 1.00)
+        let grayColor = UIColor(red: 0.21, green: 0.21, blue: 0.21, alpha: 1.00)
+        let greenColor = UIColor(red: 0.34, green: 0.58, blue: 0.46, alpha: 1.00)
+
+        view.backgroundColor = darkColor
     }
 }
