40869c52fc33dc26abdfe867ec9c4ac31c30a4fb
Michal Pietrzak <michal.pietrzak18@gmail.com>
2020-12-26 15:47:11 +0100

    adding SettingsTableViewCell xib + constraints

---
+++ Profile_Settings/Profile_Settings/SettingsTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsTableViewCell.swift
+//  Profile_Settings
+//
+//  Created by Michal Pietrzak on 26/12/2020.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
